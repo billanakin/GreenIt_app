d537d3e072f00424cd8598f33ad3e2d8190f2560
Bill Anakin Zamora <bill.anakin143@gmail.com>
2024-01-21 00:03:21 +0800

    Added assets for map display sheet

--- lib/components/sheets/map_display_modal_bottom_sheet.dart
+++ lib/components/sheets/map_display_modal_bottom_sheet.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:greenit_version1/components/posts/section_header.dart';
+import 'package:greenit_version1/constants.dart';
+import 'package:greenit_version1/size_config.dart';
 
 Future mapDisplayModalBottomSheet(BuildContext context) {
   return showModalBottomSheet(
     context: context,
     builder: (context) => SafeArea(
       child: Container(
-        height: 250,
-        width: double.infinity,
-        padding:
-            const EdgeInsets.only(top: 25, left: 20, right: 20, bottom: 20),
+        height: getProportionateScreenHeight(250),
+        width: SizeConfig.screenWidth,
+        padding: EdgeInsets.symmetric(
+          horizontal: kDefaultHorizontalPadding,
+          vertical: kSecondaryVerticalPadding,
+        ),
+        decoration: const ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+            ),
+          ),
+          shadows: [
+            BoxShadow(
+              color: Color(0x21000000),
+              blurRadius: 6,
+              offset: Offset(0, 0),
+              spreadRadius: 0,
+            )
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SectionHeader(
+              title: 'Choose Map',
+              subtitle: 'Personalize your map visuals',
+            ),
+            const VerticalSpacing(of: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: getProportionateScreenWidth(170),
+                  height: getProportionateScreenHeight(140),
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        width: 1,
+                        color: kPrimaryBorderColor,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Stack(
+                    clipBehavior: Clip.antiAlias,
+                    children: [
+                      Container(color: Colors.green),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     ),
   );
