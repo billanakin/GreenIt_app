d522826461687aba9e7d4ac21fb39965263ed125
Bill Anakin Zamora <bill.anakin143@gmail.com>
2024-01-21 17:53:16 +0800

    Extracted and Refined Widgets

--- lib/screens/newPost/components/body.dart
+++ lib/screens/newPost/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:greenit_version1/components/cards/warning_info_card.dart';
 import 'package:greenit_version1/components/profile/profile_avatar.dart';
 import 'package:greenit_version1/constants.dart';
 import 'package:greenit_version1/data/profile_data.dart';
@@ -30,12 +31,6 @@ class _BodyState extends State<Body> {
     _postTitleNode!.dispose();
     _postMessageNode!.dispose();
   }
-
-  /*
-    focusNode: _focusNode,
-  textInputAction: TextInputAction.done,
-  onEditingComplete: () => _focusNode.unfocus(),
-  */
 
   @override
   Widget build(BuildContext context) {
@@ -83,135 +78,119 @@ class _BodyState extends State<Body> {
                   ],
                 ),
                 const VerticalSpacing(of: 30),
-                TextField(
-                  focusNode: _postTitleNode,
-                  textInputAction: TextInputAction.done,
-                  onEditingComplete: () => _postTitleNode!.unfocus(),
-                  onChanged: (value) => postTitle = value, // POST TITLE HERE
-                  style: kPrimaryBodyTextStyle.copyWith(
-                    fontFamily: 'Helvetica',
-                    fontWeight: FontWeight.w600,
-                  ),
-                  cursorColor: kPrimaryActiveColor,
-                  maxLines: null,
-                  maxLength: 75,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    contentPadding: EdgeInsets.zero,
-                    isDense: true,
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                    hintText: 'Consider your title...',
-                    hintStyle: kPrimaryBodyTextStyle.copyWith(
-                      fontFamily: 'Helvetica',
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
+                buildPostTitleField(),
                 const VerticalSpacing(of: 5),
-                TextField(
-                  focusNode: _postMessageNode,
-                  textInputAction: TextInputAction.done,
-                  onEditingComplete: () => _postMessageNode!.unfocus(),
-                  onChanged: (value) =>
-                      postMessage = value, // POST MESSAGE HERE
-                  style: kPrimaryBodyTextStyle,
-                  cursorColor: kPrimaryActiveColor,
-                  maxLines: null,
-                  maxLength: 750,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    contentPadding: EdgeInsets.zero,
-                    isDense: true,
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                    hintText: "Keep us in the loop on what's happening...",
-                    hintStyle: kPrimaryBodyTextStyle,
-                  ),
-                ),
+                buildPostMessageField(),
                 const VerticalSpacing(of: 20),
-                InkWell(
-                  onTap: () {}, // ADD access to gallery here
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        ...List.generate(
-                          4,
-                          (index) => Padding(
-                            padding: const EdgeInsets.only(right: 5),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: SizedBox(
-                                height: getProportionateScreenHeight(180),
-                                child: AspectRatio(
-                                  aspectRatio: 4 / 5,
-                                  child: Container(
-                                    decoration: ShapeDecoration(
-                                      shape: RoundedRectangleBorder(
-                                        side: const BorderSide(
-                                            width: 1, color: Color(0x66868686)),
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                    ),
-                                    child: const Center(
-                                      child: Icon(
-                                        Icons.insert_photo_outlined,
-                                        color: kPrimaryBorderColor,
-                                        size: 75,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                buildInputPhotoCard(),
                 const VerticalSpacing(of: 20),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: kDefaultHorizontalPadding,
-                    vertical: kSecondaryVerticalPadding,
-                  ),
-                  decoration: ShapeDecoration(
-                    color: const Color(0x26868686),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Icon(
-                        Icons.error_rounded,
-                        color: Color(0xFF868686),
-                      ),
-                      const HorizontalSpacing(of: 10),
-                      Expanded(
-                        child: Text(
-                          'To ensure your post is truthful, enhance it by including a relevant photograph.',
-                          style: kSecondaryBodyTextStyle.copyWith(
-                            color: kPrimaryBodyTextColor,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
+                const WarningInfoCard(
+                  info:
+                      'To ensure your post is truthful, enhance it by including a relevant photograph.',
+                ),
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  InkWell buildInputPhotoCard() {
+    return InkWell(
+      onTap: () {}, // ADD access to gallery here
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            ...List.generate(
+              4,
+              (index) => Padding(
+                padding: const EdgeInsets.only(right: 5),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: SizedBox(
+                    height: getProportionateScreenHeight(180),
+                    child: AspectRatio(
+                      aspectRatio: 4 / 5,
+                      child: Container(
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            side: const BorderSide(
+                                width: 1, color: Color(0x66868686)),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: const Center(
+                          child: Icon(
+                            Icons.insert_photo_outlined,
+                            color: kPrimaryBorderColor,
+                            size: 75,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  TextField buildPostMessageField() {
+    return TextField(
+      focusNode: _postMessageNode,
+      textInputAction: TextInputAction.done,
+      onEditingComplete: () => _postMessageNode!.unfocus(),
+      onChanged: (value) => postMessage = value, // POST MESSAGE HERE
+      style: kPrimaryBodyTextStyle,
+      cursorColor: kPrimaryActiveColor,
+      maxLines: null,
+      maxLength: 750,
+      decoration: InputDecoration(
+        fillColor: Colors.white,
+        contentPadding: EdgeInsets.zero,
+        isDense: true,
+        border: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        enabledBorder: InputBorder.none,
+        errorBorder: InputBorder.none,
+        disabledBorder: InputBorder.none,
+        hintText: "Keep us in the loop on what's happening...",
+        hintStyle: kPrimaryBodyTextStyle,
+      ),
+    );
+  }
+
+  TextField buildPostTitleField() {
+    return TextField(
+      focusNode: _postTitleNode,
+      textInputAction: TextInputAction.done,
+      onEditingComplete: () => _postTitleNode!.unfocus(),
+      onChanged: (value) => postTitle = value, // POST TITLE HERE
+      style: kPrimaryBodyTextStyle.copyWith(
+        fontFamily: 'Helvetica',
+        fontWeight: FontWeight.w600,
+      ),
+      cursorColor: kPrimaryActiveColor,
+      maxLines: null,
+      maxLength: 75,
+      decoration: InputDecoration(
+        fillColor: Colors.white,
+        contentPadding: EdgeInsets.zero,
+        isDense: true,
+        border: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        enabledBorder: InputBorder.none,
+        errorBorder: InputBorder.none,
+        disabledBorder: InputBorder.none,
+        hintText: 'Consider your title...',
+        hintStyle: kPrimaryBodyTextStyle.copyWith(
+          fontFamily: 'Helvetica',
+          fontWeight: FontWeight.w600,
         ),
       ),
     );
