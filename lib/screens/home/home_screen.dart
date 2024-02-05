e14d14982d6e7b2ce9e758d124333e2c56292e96
Bill Anakin Zamora <bill.anakin143@gmail.com>
2024-02-05 11:08:55 +0800

    Added Home Screen Loading

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:greenit_app/apis/post_api.dart';
 import 'package:greenit_app/components/app_bar/home_page_app_bar.dart';
 import 'package:greenit_app/components/buttons/add_new_post_button.dart';
 import 'package:greenit_app/models/current.dart';
+import 'package:greenit_app/models/post.dart';
 import 'package:greenit_app/screens/home/components/body.dart';
 
 import 'package:greenit_app/models/profile.dart';
+import 'package:greenit_app/screens/home/loading/home_screen_loading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Future<List<Post>> _loadData() async {
+    var apiResponse = await PostApi().latest();
+    if (apiResponse.success) {
+      return apiResponse.data!.list;
+    } else {
+      return <Post>[];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<List<Post>>(
+      future: _loadData(),
+      builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+        if (snapshot.hasData) {
+          return buildWidgets(context, snapshot.data!);
+        } else {
+          return const HomeScreenLoading();
+        }
+      },
+    );
+  }
+
+  PopScope buildWidgets(BuildContext context, List<Post> data) {
     return PopScope(
       canPop: false,
-      child: SafeArea(
-        child: Scaffold(
-          extendBodyBehindAppBar: true,
-          appBar: HomePageAppBar(
-            userProfile: Profile.fromUser(Current.user!),
-          ),
-          body: const Body(),
-          floatingActionButton: const AddNewPostButton(),
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: HomePageAppBar(
+          userProfile: Profile.fromUser(Current.user!),
         ),
+        body: Body(posts: data),
+        floatingActionButton: const AddNewPostButton(),
       ),
     );
   }
